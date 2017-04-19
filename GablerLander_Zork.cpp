57375941cf8a4b95036b7bf9caf07112a7be8bdd
Jon Lander <lander.jon9@gmail.com>
2017-04-19 12:01:22 -0400

    Created the project

---
+++ GablerLander_Zork.cpp
@@ -0,0 +1,16 @@
+/*
+Author: John Gabler, Jonathan Lander
+Project: Zork Adventure
+*/
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout <<"test" << endl;
+
+    return 0;
+}
